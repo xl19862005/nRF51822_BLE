0949bbdf54de4e933899c4daf10b2c044b5d275c
Xandy.Xiong <xl19862005@126.com>
2015-09-07 23:17:03 +0800

    增加第一个版本代码 Xandy 2015.09.07

---
+++ puk208/s130/arm5/RTE/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_uart_s130_pca10028' 
+ * Target:  'nrf51422_xxac_s130' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS=1>nrf_drv_gpiote
+#define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS=4>nrf_drv_gpiote
+#define GPIOTE_ENABLED=1>.
+  #define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS=1>.
+#define S130
+#define SOFTDEVICE_PRESENT
+#define SWI_DISABLE0
+
+#endif /* RTE_COMPONENTS_H */
