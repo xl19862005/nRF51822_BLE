7488ed73bd98a108ef6d58814a230cd87f540e83
Xandy.Xiong <xandy.xiong@anchormobile.com>
2015-09-08 18:08:52 +0800

    拆分main文件中的代码 让代码整洁 Xandy 2015.09.08

---
+++ include/app_board.h
@@ -0,0 +1,34 @@
+#ifndef _APP_BOARD_H_
+#define _APP_BOARD_H_
+#include <stdint.h>
+#include <string.h>
+#include "nordic_common.h"
+#include "nrf.h"
+#include "nrf51_bitfields.h"
+#include "ble_hci.h"
+#include "ble_advdata.h"
+#include "ble_advertising.h"
+#include "ble_conn_params.h"
+#include "softdevice_handler.h"
+#include "app_timer.h"
+#include "app_button.h"
+#include "ble_nus.h"
+#include "app_uart.h"
+#include "app_util_platform.h"
+#include "bsp.h"
+#include "bsp_btn_ble.h"
+
+#define APP_TIMER_PRESCALER             0                                           /**< Value of the RTC1 PRESCALER register. */
+#define APP_TIMER_MAX_TIMERS            (2 + BSP_APP_TIMERS_NUMBER)                 /**< Maximum number of simultaneously created timers. */
+#define APP_TIMER_OP_QUEUE_SIZE         4                                           /**< Size of timer operation queues. */
+
+#define UART_TX_BUF_SIZE                256                                         /**< UART TX buffer size. */
+#define UART_RX_BUF_SIZE                256                                         /**< UART RX buffer size. */
+
+
+uint16_t                         m_conn_handle = BLE_CONN_HANDLE_INVALID;    /**< Handle of the current connection. */
+ble_nus_t                        m_nus;                                      /**< Structure to identify the Nordic UART Service. */
+
+void app_board_init(void);
+
+#endif
---
+++ include/app_io.h
@@ -0,0 +1,6 @@
+#ifndef _APP_IO_H
+#define _APP_IO_H
+#include "app_board.h"
+
+void buttons_leds_init(bool * p_erase_bonds);
+#endif
